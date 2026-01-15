fa3d122b12939122c5a57cb907aa7d00c5804ddf
karthikeyanmuthukumaranm <karthi.ac.kdp@gmail.com>
2026-01-15 14:26:37 +0530

    Add files via upload

---
+++ ArrayListExampl.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListExampl{
+    public static void main(String[] args) {
+        ArrayList<Integer> List = new ArrayList<>();
+        List.add(10);
+        List.add(20);
+        List.add(30);
+
+        System.out.println("ArrayList elements: " + List);
+    }   
+}
